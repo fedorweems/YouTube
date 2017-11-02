cbd1f37fa673dc70218a05c9a22ae3f0530f36c0
Fedor Weems <fedorweems@aim.com>
2017-11-02 13:25:20 -0500

    Serial Communication with 2 arduinos
    Send serial commands to arduino.

---
+++ Serial_Receiver.ino
@@ -0,0 +1,27 @@
+
+String Message;
+
+void setup() {
+  
+  Serial.begin(9600);
+  Serial.setTimeout(50); // default is 1 second
+  delay(100);
+ pinMode(3, OUTPUT);
+
+} // setup
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+if (Serial.available()) {
+    Message = Serial.readString(); }
+
+
+delay(4);
+if (Message == "on"){
+  digitalWrite(3, HIGH); }
+if (Message == "off"){
+  digitalWrite(3, LOW); }  
+//Message = "\0";
+}   // loop
+
---
+++ Serial_Transmiter.ino
@@ -0,0 +1,32 @@
+
+
+String Message;
+
+void setup() {
+  
+  Serial.begin(9600);
+  delay(100);
+
+
+
+
+}
+
+void loop() {
+  
+  
+  Serial.print("on");
+  delay(2500);
+  Serial.print("off");
+  delay(2500);
+
+  
+if (Serial.available()) {
+    Message = Serial.readString();  }
+
+//Serial.println(Message);
+
+ 
+
+
+}
