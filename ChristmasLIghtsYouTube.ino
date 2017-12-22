99671a4775f5f5fc33a9ad54dcf78c9dba2beedd
Fedor Weems <fedorweems@aim.com>
2017-12-22 15:19:02 -0600

    Add files via upload

---
+++ ChristmasLIghtsYouTube.ino
@@ -0,0 +1,81 @@
+
+#include "FastLED.h"
+
+// How many leds in your strip?
+#define NUM_LEDS 60
+#define TRAIL    220
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 3
+#define CLOCK_PIN 13
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup() { 
+      // Uncomment/edit one of the following lines for your leds arrangement.
+      // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+                                        //RGB
+       FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+      //FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+      // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+      
+      // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<P9813, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<APA102, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<DOTSTAR, RGB>(leds, NUM_LEDS);
+
+      // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+}
+
+void loop() { 
+ 
+  for( int i = 0; i <= NUM_LEDS; i++) {
+                      //Red, Saturation, Brightness
+        leds[i] = CHSV( 0,     255,          255);
+        FastLED.show();
+       delay(50);
+       fadeall();
+    }
+  for( int i = NUM_LEDS; 0 <= i; i--) {
+                      //Green, Saturation, Brightness
+        leds[i] = CHSV( 85,       255,       255);
+        FastLED.show();
+        delay(50);
+        fadeall();
+        }//for
+        } //loop
+
+
+// fades the LED's behind the leading LED
+void fadeall() { 
+  for(int i = 0; i < NUM_LEDS; i++) { 
+    leds[i].nscale8(TRAIL); } 
+    }
+
+//   0 Red
+//  42 Yellow
+//  85 Green
+// 128 Aqua
+// 171 Blue
+// 213 Purple
+// https://github.com/FastLED/FastLED/wiki/Pixel-reference
+
+
---
+++ LED_StripColorExamples.ino
@@ -0,0 +1,21 @@
+
+
+//Example 1: set color from red, green, and blue components individually
+  leds[i].red =    50; // 0-255
+  leds[i].green = 100;
+  leds[i].blue =  150;
+
+  //Example 2: set color from red, green, and blue components all at once
+  leds[i] = CRGB( 50, 100, 150);
+
+  // Example 3: set color via 'hex color code' (0xRRGBB)
+  leds[i] = 0x0000FF;
+
+  // Example 4: set color via any named HTML web color
+  leds[i] = CRGB::HotPink;
+
+  // Example 5: set color via setRGB
+  leds[i].setRGB( 50, 100, 150);
+  
+ // Example 6    color,  saturation, brightness
+  leds[i] = CHSV( 85,       255,           255);
